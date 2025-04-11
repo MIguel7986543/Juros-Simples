40a95402485060684af7757f5dd1d5f869093dbf
Miguel Dias <mdias1300@gmail.com>
2025-04-11 20:12:51 -0300

    não consegui terminar

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+// j - c. i. t
+// m - t.j
+
+decimal capital, juros, montante;
+double taxa;
+int tempo;
+
+Console.WriteLine("juros simples (j)\n");
+
+Console.Write("capital [c] (R$).........:\n");
+
+capital = Convert.ToDecimal(Console.ReadLine());
+Console.Write("taxa juros [i] (%).......:\n");
+taxa = Convert.ToDouble(Console.ReadLine());
+Console.Write("tempo [t] (meses)........:\n");
+tempo = Convert.ToInt32(Console.ReadLine());
+ 
+
+Console.WriteLine($"\nJuros (R$).....: 360,00");
+Console.WriteLine($"Montante (R$)..: 1560,00");
